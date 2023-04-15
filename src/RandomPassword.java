40b44d56e80554adb91f5bb1fd72eb5ea330a21b
CatPerson136 <duckingyou69@gmail.com>
2023-04-15 14:45:32 -0400

    re-did the password file using a random string algrithom from https://www.geeksforgeeks.org/generate-random-string-of-given-size-in-java/

--- src/RandomPassword.java
+++ src/RandomPassword.java
@@ -1,29 +1,31 @@
-// This will import the random library
-import java.util.Random;
-
-// This will call the array library
-import java.util.Arrays;
-
 // This is the start of the class
 public class RandomPassword {
-    // This starts the Random class and makes it public
-    public static Random random = new Random();
+    /*
+     * Thanks to Geeks or Geeks for the code.
+     * The code can be found here:
+     * https://www.geeksforgeeks.org/generate-random-string-of-given-size-in-java/
+     * I also modfiyed it to work in here.
+     */
+    static String getAlphaNumericString(int n) {
+        // choose a Character random from this String
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz"
+                + "!@#$%^&*{}:w?";
+        // create StringBuffer size of AlphaNumericString
+        StringBuilder sb = new StringBuilder(n);
+        for (int i = 0; i < n; i++) {
+            // generates a random number between
+            // 0 to AlphaNumericString variable length
+            int index = (int) (AlphaNumericString.length() * Math.random());
+            // add Character one by one in end of sb
+            sb.append(AlphaNumericString.charAt(index));
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
-        // This is the begining of the file.
-        // System.out.println("File started"); // This is to check if the file is running
-        // Function to make a random string 
-        randomInt(); // This is the function that is called to genrate a random integer
-    }
-    public static void randomInt() {
-       int randomInt = random.nextInt(20); // This will genrate a random int with a maxium value of 6
-       System.out.println(randomInt);
-    }
-    // TODO: This will genrate a random string
-    public static void randomString() {
-        
-    }
-    // TODO: This will add to an array
-    public static void addToArray() {
-        
+        // Gets the string function add displays it
+        System.out.println(RandomPassword.getAlphaNumericString(30));
     }
 }
