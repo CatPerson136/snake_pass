6926334368dfd8dd25360cc28ff665e342e151f5
CatPerson136 <duckingyou69@gmail.com>
2023-04-15 14:45:47 -0400

    re-did the password file using a random string algrithom from https://www.geeksforgeeks.org/generate-random-string-of-given-size-in-java/

--- src/RandomPassword.java
+++ src/RandomPassword.java
@@ -4,21 +4,21 @@ public class RandomPassword {
      * Thanks to Geeks or Geeks for the code.
      * The code can be found here:
      * https://www.geeksforgeeks.org/generate-random-string-of-given-size-in-java/
-     * I also modfiyed it to work in here.
+     * I also modfied it to add other characters as well.
      */
     static String getAlphaNumericString(int n) {
-        // choose a Character random from this String
+        // Choose a Character random from this String
         String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                 + "0123456789"
                 + "abcdefghijklmnopqrstuvxyz"
                 + "!@#$%^&*{}:w?";
-        // create StringBuffer size of AlphaNumericString
+        // Creates StringBuffer the size of AlphaNumericString
         StringBuilder sb = new StringBuilder(n);
         for (int i = 0; i < n; i++) {
-            // generates a random number between
+            // Generates a random number between
             // 0 to AlphaNumericString variable length
             int index = (int) (AlphaNumericString.length() * Math.random());
-            // add Character one by one in end of sb
+            // Add character one by one in end of sb
             sb.append(AlphaNumericString.charAt(index));
         }
         return sb.toString();
