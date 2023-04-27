725dee98589f195e895ac25e12a1938a17675fa6
CatPerson136 <duckingyou69@gmail.com>
2023-04-27 19:24:22 -0400

    fixed on line 29

--- src/main.py
+++ src/main.py
@@ -10,11 +10,10 @@
 
 import customtkinter as ctk
 import subprocess as sb
-import os
 
 
 # This will run the java file then print it on a label.
-def generate_button():
+def generate_string():
     output = sb.check_output(["java", "com/cat/RandomPassword.java"])
 
 
@@ -27,14 +26,14 @@ window.geometry("300x300")  # this is window size
 window.title("Snake Pass")
 """
 * This button will start the java file.
-* Which is KeyReader.java
+* Which is RandomPassword.java
 """
-btn = ctk.CTkButton(master=window, text="Start", command=generate_button).place(
+btn = ctk.CTkButton(master=window, text="Start", command=generate_string).place(
     relx=0.5, rely=0.5, anchor=ctk.CENTER
 )
 
 # This the label to capture the key inputs and display them.
-output_label = ctk.CTkLabel(master=window, text="Pass").place(
+output_label = ctk.CTkLabel(master=window, text="").place(
     relx=0.5, rely=0.3, anchor=ctk.N
 )
 
