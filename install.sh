890e98ed232a98e8ea48a27f223934e401d9fd1f
otto von internet <116551503+otto-von-internet@users.noreply.github.com>
2023-05-01 22:11:15 -0400

    updated the install.sh file so it is better and now it will work for fedora

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
-# This script will install the dependeces for program
-
+# This will install it for Ubuntu and its dirivites
 if [ -d /etc/apt ]
 then
-    sudo apt install pip
-    pip install customtkinter
-    sudo apt install python3-tk
-    pip install py4j
+    sudo apt install pip python3-tk
+    pip install customtkinter py4j
 fi
 
-# This will install it for Arch and its dirivites
+# This will install it for Arch and its dirivites (untested)
 if [ -d /etc/pacman.d ]
 then
-   sudo pacman -S python3-pip 
-   pip install customtkinter
-   sudo pacman -S tk  
-   pip install py4j
+   sudo pacman -S python3-pip tk 
+   pip install customtkinter py4j
+fi
+
+# This will install it for Fedora and its dirivites
+if [ -d /etc/dnf ]
+then
+   sudo dnf install python3-pip python3-tkinter 
+   pip install customtkinter py4j
+fi
