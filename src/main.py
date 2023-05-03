fae87a21cbc91560c6791b8d65640b7a86b94068
otto von internet <116551503+otto-von-internet@users.noreply.github.com>
2023-05-02 22:20:24 -0400

    got rid of unnecessary lines

--- src/main.py
+++ src/main.py
@@ -12,7 +12,6 @@ import customtkinter as ctk
 import tkinter as tk
 import subprocess as sb
 
-
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("blue")
 
@@ -26,11 +25,9 @@ window.title("Snake Pass")
 * Which is RandomPassword.java
 """
 
-
 # This will run the java file then print it on a label.
 def generate_string():
     output = sb.check_output(["java", "com/cat/RandomPassword.java"])
-    # This the label to capture the key inputs and display them.
     f = output.decode("utf-8")
     str_var = tk.StringVar()
     str_var.set(f)
@@ -38,10 +35,8 @@ def generate_string():
         master=window, textvariable=str_var, width=250, height=40
     ).place(relx=0.5, rely=0.3, anchor=ctk.N)
 
-
 btn = ctk.CTkButton(master=window, text="Generate", command=generate_string).place(
     relx=0.5, rely=0.5, anchor=ctk.CENTER
 )
-
 
 window.mainloop()
