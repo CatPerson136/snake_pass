77846072e7c06ea28dc5f666e314d8ef407d2e53
otto von internet <116551503+otto-von-internet@users.noreply.github.com>
2023-05-02 21:51:15 -0400

    updated the install.sh file

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 if [ -d /etc/apt ]
 then
     sudo apt install pip python3-tk
-    pip install customtkinter py4j
+    pip install customtkinter 
 fi
 
 # This will install it for Arch and its dirivites (untested)
 if [ -d /etc/pacman.d ]
 then
    sudo pacman -S python3-pip tk 
-   pip install customtkinter py4j
+   pip install customtkinter 
 fi
 
 # This will install it for Fedora and its dirivites
 if [ -d /etc/dnf ]
 then
    sudo dnf install python3-pip python3-tkinter 
-   pip install customtkinter py4j
+   pip install customtkinter 
 fi
