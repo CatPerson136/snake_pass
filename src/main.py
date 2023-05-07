85a451ce688951997cb24652a41259a419fffc47
CatPerson136 <duckingyou69@gmail.com>
2023-05-07 13:09:06 -0400

    added a function to set a password and print it out to the console

--- src/main.py
+++ src/main.py
@@ -8,9 +8,9 @@
 
 ####################################
 
-from typing import Optional, Tuple, Union
 import customtkinter as ctk
-from password import Password
+import random
+import string
 
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("blue")
@@ -24,8 +24,19 @@ class App(ctk.CTk):
         self.geometry("350x300")  # this is the size of the window
 
         self.btn = ctk.CTkButton(
-            self, text="Generate", command=Password.generate_string
+            self, text="Generate", command=App.set_password_and_print_it_out
         ).place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
+
+    def set_password_and_print_it_out():
+        # gets 8 random ascii charters.
+        random_string = "".join(random.choices(string.ascii_letters, k=8))
+
+        # gets 4 random numbers.
+        random_int = "".join(random.choices(string.digits, k=4))
+
+        # Adds it to a f string and prints it out.
+        password = f"{random_string}{random_int}"
+        print(password)
 
 
 app = App()
