927c91c339617ee93a57455ac028682342b28dc4
CatPerson136 <duckingyou69@gmail.com>
2023-05-07 12:00:44 -0400

    adding a way to add a random string and a random int

--- src/password.py
+++ src/password.py
@@ -1,9 +1,12 @@
 import customtkinter as ctk
 import random
+import string
 
 
-class Password:
-    # This will run the java file then print it on a label.
-    def generate_string():
-        r = random.randint(1, 100)
-        output = ctk.CTkEntry(master=window, width=160, height=40, text=r).pack()
+class RandomPassword:
+    def __init__(self) -> None:
+        self.password = random.choices(string.ascii_letters) + str(random.randint(0, 9))
+        print(self.password)
+
+
+RandomPassword()
