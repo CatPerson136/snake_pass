00724fec841ee558352f23dac31ecbabd46e0b43
CatPerson136 <duckingyou69@gmail.com>
2023-05-08 17:42:56 -0400

    git the password generation to work

--- src/main.py
+++ src/main.py
@@ -9,12 +9,28 @@
 ####################################
 
 import customtkinter as ctk
-import tkinter as tk
 import random
 import string
+from hashlib import sha512
+import tkinter as tk
 
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("blue")
+
+
+def set_password_and_print_it_out():
+    # Gets 8 random ascii charters and adds to a string.
+    password = sha512(
+        f"{random.choices(string.ascii_letters, k=8)}".encode("utf-8")
+    ).hexdigest()
+
+    password_to_string = tk.StringVar()
+    password_to_string.set(password)
+
+    # A text entry to print the password and allows it to be copied.
+    password_text_entry = ctk.CTkEntry(
+        master=app, width=160, height=40, textvariable=password_to_string
+    ).place(relx=0.5, rely=0.3, anchor=ctk.CENTER)
 
 
 class App(ctk.CTk):
@@ -26,29 +42,9 @@ class App(ctk.CTk):
 
         # A button to generate a radom password when clicked.
         self.btn = ctk.CTkButton(
-            self, text="Generate", command=App.set_password_and_print_it_out
+            self, text="Generate", command=set_password_and_print_it_out
         ).place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
-
-    def set_password_and_print_it_out(self):
-        # Gets 8 random ascii charters and adds to a string.
-        self.random_string = "".join(random.choices(string.ascii_letters, k=8))
-
-        # Gets 4 random numbers and adds to a string.
-        self.random_int = "".join(random.choices(string.digits, k=4))
-
-        # Adds it to an f string and prints it out.
-        self.password = f"{str(self.random_string)}{str(self.random_int)}"
-
-        # Converting self.password to a StringVar from tkinter.
-        self.password_to_string = tk.StringVar()
-        self.password_to_string.set(self.password)
-
-        # A text entry to print the password and allows it to be copied.
-        self.password_text_entry = ctk.CTkEntry(
-            self, width=160, height=40, textvariable=self.password_to_string
-        ).place(relx=0.5, rely=0.3, anchor=ctk.CENTER)
 
 
 app = App()
-app.set_password_and_print_it_out()
 app.mainloop()
