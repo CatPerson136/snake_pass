7240f1693c1cbf54006a94a372927da64b8a1f07
CatPerson136 <duckingyou69@gmail.com>
2023-05-08 17:55:06 -0400

    added self to the function calling on line 29/30

--- src/main.py
+++ src/main.py
@@ -18,21 +18,6 @@ ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("blue")
 
 
-def set_password_and_print_it_out():
-    # Gets 8 random ascii charters and adds to a string.
-    password = sha512(
-        f"{random.choices(string.ascii_letters, k=8)}".encode("utf-8")
-    ).hexdigest()
-
-    password_to_string = tk.StringVar()
-    password_to_string.set(password)
-
-    # A text entry to print the password and allows it to be copied.
-    password_text_entry = ctk.CTkEntry(
-        master=app, width=160, height=40, textvariable=password_to_string
-    ).place(relx=0.5, rely=0.3, anchor=ctk.CENTER)
-
-
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -42,8 +27,22 @@ class App(ctk.CTk):
 
         # A button to generate a radom password when clicked.
         self.btn = ctk.CTkButton(
-            self, text="Generate", command=set_password_and_print_it_out
+            self, text="Generate", command=self.set_password_and_print_it_out
         ).place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
+
+    def set_password_and_print_it_out(self):
+        # Gets 8 random ascii charters and adds to a string.
+        self.password = sha512(
+            f"{random.choices(string.ascii_letters, k=8)}".encode("utf-8")
+        ).hexdigest()
+
+        self.password_to_string = tk.StringVar()
+        self.password_to_string.set(self.password)
+
+        # A text entry to print the password and allows it to be copied.
+        self.password_text_entry = ctk.CTkEntry(
+            master=app, width=160, height=40, textvariable=self.password_to_string
+        ).place(relx=0.5, rely=0.3, anchor=ctk.CENTER)
 
 
 app = App()
