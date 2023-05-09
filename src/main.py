b57697eb133b8069b1beb36963902645d9202d00
otto von internet <116551503+otto-von-internet@users.noreply.github.com>
2023-05-08 22:02:29 -0400

    improved the generation of passwords + added a copy button

--- src/main.py
+++ src/main.py
@@ -11,12 +11,10 @@
 import customtkinter as ctk
 import random
 import string
-from hashlib import sha512
 import tkinter as tk
 
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("blue")
-
 
 class App(ctk.CTk):
     def __init__(self):
@@ -28,22 +26,29 @@ class App(ctk.CTk):
         # A button to generate a radom password when clicked.
         self.btn = ctk.CTkButton(
             self, text="Generate", command=self.set_password_and_print_it_out
-        ).place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
+        ).place(relx=0.28, rely=0.5, anchor=ctk.CENTER)
 
+        # A button to copy the password to your clipboard.
+        self.copy_btn = ctk.CTkButton(
+            self, text="Copy", command=self.copy_password_to_clipboard
+        ).place(relx=0.72, rely=0.5, anchor=ctk.CENTER)
+
+    def copy_password_to_clipboard(self):
+        # Copies the generated password to the clipboard.
+        self.clipboard_clear()
+        self.clipboard_append(self.password)
+
+    # Gets 32 random ascii characters (uppercase, lowercase, digits, and punctuation)
     def set_password_and_print_it_out(self):
-        # Gets 8 random ascii charters and adds to a string.
-        self.password = sha512(
-            f"{random.choices(string.ascii_letters, k=8)}".encode("utf-8")
-        ).hexdigest()
+        self.password = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation, k=32))
 
         self.password_to_string = tk.StringVar()
         self.password_to_string.set(self.password)
 
         # A text entry to print the password and allows it to be copied.
         self.password_text_entry = ctk.CTkEntry(
-            master=app, width=160, height=40, textvariable=self.password_to_string
+            master=app, width=260, height=40, textvariable=self.password_to_string
         ).place(relx=0.5, rely=0.3, anchor=ctk.CENTER)
-
 
 app = App()
 app.mainloop()
