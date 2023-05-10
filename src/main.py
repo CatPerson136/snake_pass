b617c79d6d0b033d785a66496bb0ed12b3a4f8c6
Andrew Smith <92001731+CatPerson136@users.noreply.github.com>
2023-05-09 21:26:59 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,10 +2,7 @@
 
 # Snake Pass
 
-# Copyright (C) 2023 Andrew Smith. GNU General Public License v3
-
-# License: https://www.gnu.org/licenses/gpl-3.0.en.html
-
+# Copyright (C) 2023 Andrew Smith. MIT
 ####################################
 
 import customtkinter as ctk
